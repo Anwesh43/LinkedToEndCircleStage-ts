7f01dbfbc8d2d8c01683abfa3098577d103b08c5
Anwesh43 <anweshthecool0@gmail.com>
2018-10-02 20:00:23 +0530

    adding LTENode class

--- LineToEndCircleStage.ts
+++ LineToEndCircleStage.ts
@@ -71,3 +71,74 @@ class Animator {
         }
     }
 }
+
+class LTENode {
+
+    next : LTENode
+    prev : LTENode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LTENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = h / (nodes + 1)
+        const r : number = gap / 3
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.strokeStyle = '#4CAF50'
+        context.save()
+        context.translate(w/2, this.i * gap + gap)
+        for (var j = 0; j < 2; j++) {
+            const sf : number = 1 - 2 * j
+            const sc : number = Math.min(0.5, Math.max(this.state.scale - 0.5, 0)) * 2
+            const sc1 : number = Math.min(0.5, sc) * 2
+            const sc2 : number = Math.min(0.5, Math.max(sc - 0.5, 0)) * 2
+            context.save()
+            context.scale(sf, 1)
+            context.translate((w/2 - r) * sc2, 0)
+            context.rotate(Math.PI * sc2)
+            context.beginPath()
+            for(var i = -90; i <= 90; i++) {
+                const x = r * sc1 * Math.cos(i * Math.PI/180)
+                const y = r * Math.sin(i * Math.PI/180)
+                if (i == -90) {
+                    context.moveTo(x, y)
+                } else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LTENode {
+        var curr : LTENode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
